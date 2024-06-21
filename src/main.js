55c83a2210d18882a406e2929b324ac3009238cb
Lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2024-06-22 01:56:52 +0400

    almost stable except notifications

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedState from "pinia-plugin-persistedstate"
+
 
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).use(autoAnimatePlugin).use(createPinia()).mount("#app");
+createApp(App).use(router).use(autoAnimatePlugin).use(createPinia().use(piniaPluginPersistedState)).mount("#app");
